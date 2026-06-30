62a6bf270a46bf563cda055807e92d16f702daa6
Leidy Bautista <analistaintegradordgics2@gmail.com>
2026-06-30 11:44:44 -0500

    permisos y menus

--- apps/public/menus.py
+++ apps/public/menus.py
@@ -26,45 +26,7 @@ MENUS = [
 
         ]
     },
-    {
-        "codigo": "parametrizacion",
-        "titulo": "Parametrización",
-        "icono": "parametrizacion.png",
-        "permiso": "view_menu_parametrizacion",
-        "orden": "5",
-
-        "children": [
-            {
-                "codigo": "parametros_generales",
-                "titulo": "Generales",
-                "permiso": "view_menu_parametrizacion_generales",
-                "ruta": "parametrizacion",
-                "orden": "1",
-            },
-            {
-                "codigo": "parametros_administrativos",
-                "titulo": "Administrativos",
-                "permiso": "view_menu_parametrizacion_administrativos",
-                "ruta": "parametrizacion",
-                "orden": "2",
-
-                "children": [
-
-                    {
-                        "codigo": "parametros_administrativos_conceptos_causacion",
-                        "titulo": "Conceptos Causacion",
-                        "permiso": "view_menu_parametrizacion_administrativos_conceptos_causacion",
-                        "ruta": "conceptos_causacion",
-                        "orden": "1",
-                        "acciones": CRUD
-                    }
-
-                ]
-            },
-
-        ]
-    },
-
+    
 ]
 
 
