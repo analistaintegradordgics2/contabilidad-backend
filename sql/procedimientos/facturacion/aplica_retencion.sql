94cddbb25d7bda6552206f533520d76d83f4ee6f
Leidy Bautista <analistaintegradordgics2@gmail.com>
2026-07-10 17:28:11 -0500

    ajuste en aplica retencion

--- sql/procedimientos/facturacion/aplica_retencion.sql
+++ sql/procedimientos/facturacion/aplica_retencion.sql
@@ -10,19 +10,16 @@ DECLARE
     v_aplica boolean := false;
 BEGIN
 
-    SELECT
-        CASE
-
-            WHEN in_tipo_retencion = 1 THEN false
-            WHEN in_tipo_retencion = 2 THEN false
-            WHEN in_tipo_retencion = 3 THEN false
-
-            ELSE false
-
-        END
+    SELECT CASE in_tipo_retencion
+        -- 1: ReteFuente, 2: ReteIVA, 3: ReteICA.
+        WHEN 1 THEN COALESCE(pt.retefuente, false)
+        WHEN 2 THEN COALESCE(pt.reteiva, false)
+        WHEN 3 THEN COALESCE(pt.reteica, false)
+        ELSE false
+    END
     INTO v_aplica
-    FROM personas_persona p
-    WHERE p.id = in_persona_id;
+    FROM personas_personatributario pt
+    WHERE pt.persona_id = in_persona_id;
 
     RETURN COALESCE(v_aplica,false);
 
